582d652645bbcd010cd99d6d16021633f9aed3eb
Jayxunbey <muxammadovjayxun@gmail.com>
2024-04-23 10:12:02 +0500

    saved

--- src/test/java/calculater/CalculaterAppTest.java
+++ src/test/java/calculater/CalculaterAppTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -21,13 +23,31 @@ class CalculaterAppTest {
     @ParameterizedTest
     @ValueSource(ints = {4, 2, 6, 2, 6})
     void sum(int a) {
-        Assertions.assertEquals(4,calculaterApp.sum(a,a));
+        Assertions.assertEquals(a+a,calculaterApp.sum(a,a));
     }
 
     @MethodSource("getStream")
     @ValueSource(ints = {4, 5, 6, 7, 6})
     void sum2(CalculaterApp.TestArguments testArguments) {
         Assertions.assertEquals(3,calculaterApp.sum(testArguments.a,testArguments.b));
+    }
+
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+            a,b
+            2,3
+            5,2
+            6,7
+            2,8
+            """,useHeadersInDisplayName = true)
+    void sum3(int a, int b) {
+        Assertions.assertEquals(a+b,calculaterApp.sum(a,b));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = {"filedata.csv"},useHeadersInDisplayName = true)
+    void sum4(int a, int b) {
+        Assertions.assertEquals(a+b,calculaterApp.sum(a,b));
     }
 
 
